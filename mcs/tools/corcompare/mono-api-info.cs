jonpryor/mono

commit c37bbef8f0acae10a6df43c805459cb2b3d2ad51

[corcompare] Use only C# known parameter type attributes for methods signatures

mcs/tools/corcompare/mono-api-info.cs
@@ -761,8 +761,8 @@ namespace CorCompare
 						method.Name = "~" + name;
 					}
 
-					// TODO: Better check
-					if (t != type && list.Any (l => l.DeclaringType != method.DeclaringType && l.Name == method.Name && l.Parameters.Count == method.Parameters.Count))
+					if (t != type && list.Any (l => l.DeclaringType != method.DeclaringType && l.Name == method.Name && l.Parameters.Count == method.Parameters.Count &&
+					                           l.Parameters.SequenceEqual (method.Parameters, new ParameterComparer ())))
 						continue;
 
 					list.Add (method);
@@ -771,7 +771,7 @@ namespace CorCompare
 				if (!fullAPI)
 					break;
 
-				if (!t.IsInterface || t.IsEnum)
+				if (t.IsInterface || t.IsEnum)
 					break;
 
 				if (t.BaseType == null || t.BaseType.FullName == "System.Object")
@@ -782,6 +782,19 @@ namespace CorCompare
 			} while (t != null);
 
 			return list.ToArray ();
+		}
+
+		sealed class ParameterComparer : IEqualityComparer<ParameterDefinition>
+		{
+			public bool Equals (ParameterDefinition x, ParameterDefinition y)
+			{
+				return x.ParameterType.Name == y.ParameterType.Name;
+			}
+
+			public int GetHashCode (ParameterDefinition obj)
+			{
+				return obj.ParameterType.Name.GetHashCode ();
+			}
 		}
 
 		static bool IsFinalizer (MethodDefinition method)
@@ -1518,15 +1531,13 @@ namespace CorCompare
 
 				ParameterDefinition info = infos [i];
 
-				string modifier;
-				if ((info.Attributes & ParameterAttributes.In) != 0)
-					modifier = "in";
-				else if ((info.Attributes & ParameterAttributes.Out) != 0)
-					modifier = "out";
-				else
-					modifier = string.Empty;
+				if (info.ParameterType.IsByReference) {
+					string modifier;
+					if ((info.Attributes & (ParameterAttributes.Out | ParameterAttributes.In)) == ParameterAttributes.Out)
+						modifier = "out";
+					else
+						modifier = "ref";
 
-				if (modifier.Length > 0) {
 					signature.Append (modifier);
 					signature.Append (" ");
 				}

mcs/tools/corcompare/mono-api-info.csproj
@@ -1,37 +1,15 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
-<Project ToolsVersion="4.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+<Project DefaultTargets="Build" ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
   <PropertyGroup>
     <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
     <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
-    <ProductVersion>8.0.30703</ProductVersion>
-    <SchemaVersion>2.0</SchemaVersion>
-    <ProjectGuid>{6446D5E1-8F4E-4692-8EF3-B1D4CA290ECF}</ProjectGuid>
+    <ProjectGuid>{804E854F-E8F5-4E2B-807A-4FAF4BE99C03}</ProjectGuid>
     <OutputType>Exe</OutputType>
     <RootNamespace>mono_api_info</RootNamespace>
     <AssemblyName>mono-api-info</AssemblyName>
-    <TargetFrameworkVersion>v4.0</TargetFrameworkVersion>
-    <FileAlignment>512</FileAlignment>
-    <FileUpgradeFlags>
-    </FileUpgradeFlags>
-    <UpgradeBackupLocation>
-    </UpgradeBackupLocation>
-    <OldToolsVersion>3.5</OldToolsVersion>
-    <PublishUrl>publish\</PublishUrl>
-    <Install>true</Install>
-    <InstallFrom>Disk</InstallFrom>
-    <UpdateEnabled>false</UpdateEnabled>
-    <UpdateMode>Foreground</UpdateMode>
-    <UpdateInterval>7</UpdateInterval>
-    <UpdateIntervalUnits>Days</UpdateIntervalUnits>
-    <UpdatePeriodically>false</UpdatePeriodically>
-    <UpdateRequired>false</UpdateRequired>
-    <MapFileExtensions>true</MapFileExtensions>
-    <ApplicationRevision>0</ApplicationRevision>
-    <ApplicationVersion>1.0.0.%2a</ApplicationVersion>
-    <IsWebBootstrapper>false</IsWebBootstrapper>
-    <UseApplicationTrust>false</UseApplicationTrust>
-    <BootstrapperEnabled>true</BootstrapperEnabled>
-    <TargetFrameworkProfile />
+    <TargetFrameworkVersion>v4.5</TargetFrameworkVersion>
+    <ProductVersion>8.0.30703</ProductVersion>
+    <SchemaVersion>2.0</SchemaVersion>
   </PropertyGroup>
   <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
     <DebugSymbols>true</DebugSymbols>
@@ -55,6 +33,9 @@
     <Reference Include="System" />
     <Reference Include="System.Core" />
     <Reference Include="System.Xml" />
+    <Reference Include="Mono.Cecil">
+      <HintPath>..\..\class\lib\net_4_x\Mono.Cecil.dll</HintPath>
+    </Reference>
   </ItemGroup>
   <ItemGroup>
     <Compile Include="AssemblyResolver.cs" />
@@ -65,18 +46,6 @@
       <Link>Options.cs</Link>
     </Compile>
   </ItemGroup>
-  <ItemGroup>
-    <BootstrapperPackage Include="Microsoft.Net.Client.3.5">
-      <Visible>False</Visible>
-      <ProductName>.NET Framework 3.5 SP1 Client Profile</ProductName>
-      <Install>false</Install>
-    </BootstrapperPackage>
-    <BootstrapperPackage Include="Microsoft.Net.Framework.3.5.SP1">
-      <Visible>False</Visible>
-      <ProductName>.NET Framework 3.5 SP1</ProductName>
-      <Install>true</Install>
-    </BootstrapperPackage>
-  </ItemGroup>
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
        Other similar extension points exist, see Microsoft.Common.targets.
@@ -85,10 +54,4 @@
   <Target Name="AfterBuild">
   </Target>
   -->
-  <ItemGroup>
-    <ProjectReference Include="..\..\..\external\cecil\Mono.Cecil.csproj">
-      <Project>{D68133BD-1E63-496E-9EDE-4FBDBF77B486}</Project>
-      <Name>Mono.Cecil</Name>
-    </ProjectReference>
-  </ItemGroup>
 </Project>

commit db5ba013c1c917b11514c941dde77f90d61aaa79

[corcompare] Restore Mono 4.6-compatible method name output
One of the wonderful workflows of
[`mono-api-info` and `mono-api-html`][0] is using them to track API
changes and associated breakage over time:

	# Have a "known good" assembly version;
	# keep reference/assembly.xml in version control
	$ mono-api-info assembly.dll > reference/assembly.xml

	# ...then on every new build, check for breakage:
	$ mono-api-info assembly.dll > new-assembly.xml
	$ mono-api-html reference/assembly.xml new-assembly.xml > assembly-diff.html
	$ if grep '>Removed' assembly-diff.html > /dev/null ; then \
		echo "ABI BREAK IN assembly.dll" ; \
		cat assembly-diff.html ; \
	fi

However, there is an unstated requirement for this workflow to work:
The *mono-api-info* file format **cannot change**.

(Or, at least, it can't change in a form that `mono-api-html` doesn't
know about.)

Unfortunately, commit c37bbef8 broke this implicit requirement,
causing the `//method/@name` value to change when by-ref parameters
are present. For example, consider this method:

	public partial class Matrix2 {
		public void Add (ref Matrix2 matrix) {...}
	}

In Mono 4.6, `mono-api-info` would produce:

	<method name="Add(OpenTK.Matrix2&amp;)" attrib="134" returntype="System.Void">
	  <parameters>
	    <parameter name="matrix" position="0" attrib="0" type="OpenTK.Matrix2&amp;" direction="ref" />
	  </parameters>
	</method>

Starting with commit c37bbef8, this instead becomes:

	<method name="Add(ref OpenTK.Matrix2&amp;)" attrib="134" returntype="System.Void">
	  <parameters>
	    <parameter name="matrix" position="0" attrib="0" type="OpenTK.Matrix2&amp;" direction="ref" />
	  </parameters>
	</method>

The `//method/@name` value changed from `Add(Matrix2&)` to
`Add(ref Matrix2&)`. `mono-api-html`, meanwhile, wasn't changed to
compensate for this, so `mono-api-html` would report a breakage:

	<h3>Type Changed: OpenTK.Matrix2</h3>
	<p>Removed methods:</p>
	<pre>

mcs/tools/corcompare/mono-api-info.cs
@@ -1543,10 +1543,12 @@ namespace CorCompare
 					if ((info.Attributes & (ParameterAttributes.Out | ParameterAttributes.In)) == ParameterAttributes.Out)
 						modifier = "out";
 					else
-						modifier = "ref";
+						modifier = string.Empty;
 
-					signature.Append (modifier);
-					signature.Append (" ");
+					if (modifier.Length > 0) {
+						signature.Append (modifier);
+						signature.Append (" ");
+					}
 				}
 
 				signature.Append (Utils.CleanupTypeName (info.ParameterType));